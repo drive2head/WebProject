136d02dba18bdb0da80550eb9a85e546a7e344ce
Alexandr Kvasnikov <uselessflower@gmail.com>
2019-10-06 19:14:33 +0300

    audio and demo visualization done

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,94 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AudioPlayerDOM from './components/AudioPlayerDOM.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AudioVizualization extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      play: false
+    };
+  }
+
+  handleChange(e) {
+    var audio = document.getElementById("audio");
+    audio.src = URL.createObjectURL(e.target.files[0]);
+    
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
+
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      x = 0;
+
+      analyser.getByteFrequencyData(dataArray);
+
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        
+        var r = barHeight + (25 * (i/bufferLength));
+        var g = 250 * (i/bufferLength);
+        var b = 50;
+
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth + 1;
+      }
+    }
+    renderFrame();
+  }
+
+  render()
+  {
+    return (
+      <div>
+        <canvas id="canvas"></canvas>
+        <input type="file" id="file" onChange={this.handleChange} />
+        <audio id="audio" controls> </audio>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render()
+  {
+    return (
+      <div className="App">
+        <AudioVizualization/>
+      </div>
+    );
+  }
 }
 
 export default App;
