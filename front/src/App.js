e6253150d71a984a95aeecc00199eea148b588f3
Alexandr Kvasnikov <uselessflower@gmail.com>
2019-10-08 01:42:57 +0300

    first post request

--- front/src/App.js
+++ front/src/App.js
@@ -5,40 +5,15 @@ import './App.css';
 import WaveSurfer from 'wavesurfer.js';
 import RegionPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js';
 import Timeline from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js';
+import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.css';
 
-// class DictorDescription extends React.Component {
-//   constructor(props)
-//   {
-//     super(props);
-//     this.state={
-//       ready:false
-//     };
-//   }
-
-//   render() 
-//   {
-//     return (
-//       <div id="dictor">
-//         <div id="mainInfo">
-//           <p> Dictor info </p>
-//           Name: <input id="dictorName" type="text"/><br/>
-//           City: <input id="dictorCity" type="text"/><br/>
-//           Country: <input id="dictorCountry" type="text"/><br/>
-//           Lang: <input id="dictorLang" type="text"/><br/>
-//         </div>
-//         <div id="optionalInfo">
-//           Defect: <input id="dictorDefect" type="text"/>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-class AudioVizualization extends React.Component {
+class App extends React.Component {
   constructor(props)
   {
     super(props);
+    this.handleSaveSoundButton = this.handleSaveSoundButton.bind(this);
+    this.handleSaveButton = this.handleSaveButton.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.createWavePlayer = this.createWavePlayer.bind(this);
     this.state = {
@@ -46,6 +21,7 @@ class AudioVizualization extends React.Component {
     };
     this.startTime = 0;
     this.endTime = 0;
+    this.sounds = [];
   }
 
   createWavePlayer(url)
@@ -102,78 +78,81 @@ class AudioVizualization extends React.Component {
     wavesurfer.load(url);
   }
 
+  handleSaveSoundButton()
+  {
+    var object = {
+      start: document.getElementById('startTime').innerHTML,
+      end: document.getElementById('endTime').innerHTML,
+      lang: document.getElementById('soundLang').value,
+      dialect: document.getElementById('soundDialect').value,
+      value: document.getElementById('soundValue').value
+    }
+    this.sounds.push(object);
+  }
+
   handleChange(e)
   {
     this.createWavePlayer(URL.createObjectURL(e.target.files[0]));
   }
 
-  render()
-  {
-    return (
-      <div>
-        <div id="waveform"></div>
-        <div id="timeline"></div>
-        <input id="slider" type="range" min="1" max="500" defaultValue="1"/>
-        <input type="file" id="file" onChange={this.handleChange} />
-      </div>
-    );
-  }
-}
-
-class App extends React.Component {
-  constructor(props)
-  {
-    super(props);
-    this.handleSaveButton = this.handleSaveButton.bind(this);
-    this.state = {
-      play: false
-    };
-  }
-
   handleSaveButton()
   {
-    return;
+    var person = {
+      name: document.getElementById('dictorName').value,
+      city: document.getElementById('dictorName').value,
+      country: document.getElementById('dictorName').value,
+      lang: document.getElementById('dictorName').value,
+      defect: document.getElementById('dictorName').value,
+    };
+    axios.post('/add_speaker', {
+      person: person,
+      sounds: this.sounds
+    });
   }
 
   render()
   {
     return (
       <div className="App">
-      <div class="container">
-        <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-          <div class="col-md-12 px-0">
-            <AudioVizualization/>
+        <div className="container">
+          <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+            <div className="col-md-12 px-0">
+              <div id="waveform"></div>
+              <div id="timeline"></div>
+              <input id="slider" type="range" min="1" max="500" defaultValue="1"/>
+              <input type="file" id="file" onChange={this.handleChange} />
+            </div>
           </div>
-        </div>
 
-        <div class="row mb-2">
-          <div class="col-md-6">
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
-                <p> Dictor info: </p>
-                Name: <input id="dictorName" type="text"/><br/>
-                City: <input id="dictorCity" type="text"/><br/>
-                Country: <input id="dictorCountry" type="text"/><br/>
-                Lang: <input id="dictorLang" type="text"/><br/>
-                Defect: <input id="dictorDefect" type="text"/>
+          <div className="row mb-2">
+            <div className="col-md-6">
+              <div className="card flex-md-row mb-4 box-shadow h-md-250">
+                <div className="card-body d-flex flex-column align-items-start">
+                  <p> Dictor info: </p>
+                  Name: <input id="dictorName" type="text"/><br/>
+                  City: <input id="dictorCity" type="text"/><br/>
+                  Country: <input id="dictorCountry" type="text"/><br/>
+                  Lang: <input id="dictorLang" type="text"/><br/>
+                  Defect: <input id="dictorDefect" type="text"/>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card flex-md-row mb-4 box-shadow h-md-250">
+                <div className="card-body d-flex flex-column align-items-start">
+                  <p> Sound info: </p>
+                  <p id="startTime">Start time: </p>
+                  <p id="endTime">End time: </p>
+                  Lang: <input id="soundLang" type="text"/><br/>
+                  Dialect: <input id="soundDialect" type="text"/><br/>
+                  Value: <input id="soundValue" type="text"/>
+                  <button id="saveData" onClick={this.handleSaveSoundButton}>Save sound</button>
+                </div>
               </div>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-              <div class="card-body d-flex flex-column align-items-start">
-                <p> Sound info: </p>
-                <p id="startTime">Start time: </p>
-                <p id="endTime">End time: </p>
-                Lang: <input id="dictorLang" type="text"/><br/>
-                Dialect: <input id="dictorLang" type="text"/><br/>
-                Value: <input id="dictorLang" type="text"/>
-              </div>
-            </div>
-          </div>
+          <button id="saveData" onClick={this.handleSaveButton}>Save</button>
         </div>
-        <button id="saveData" onClick={this.handleSaveButton}>Save</button>
-      </div>
       </div>
     );
   }
