8ea743cfa019ca98f692c64b469e54d2ad0e2fa2
Alexandr Kvasnikov <uselessflower@gmail.com>
2019-10-08 00:44:20 +0300

    form done

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,35 @@ import './App.css';
 import WaveSurfer from 'wavesurfer.js';
 import RegionPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js';
 import Timeline from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// class DictorDescription extends React.Component {
+//   constructor(props)
+//   {
+//     super(props);
+//     this.state={
+//       ready:false
+//     };
+//   }
+
+//   render() 
+//   {
+//     return (
+//       <div id="dictor">
+//         <div id="mainInfo">
+//           <p> Dictor info </p>
+//           Name: <input id="dictorName" type="text"/><br/>
+//           City: <input id="dictorCity" type="text"/><br/>
+//           Country: <input id="dictorCountry" type="text"/><br/>
+//           Lang: <input id="dictorLang" type="text"/><br/>
+//         </div>
+//         <div id="optionalInfo">
+//           Defect: <input id="dictorDefect" type="text"/>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 class AudioVizualization extends React.Component {
   constructor(props)
@@ -21,6 +50,7 @@ class AudioVizualization extends React.Component {
 
   createWavePlayer(url)
   {
+    document.getElementById('waveform').innerHTML = '';
     var wavesurfer = WaveSurfer.create({
       container: '#waveform',
       waveColor: 'red',
@@ -37,6 +67,7 @@ class AudioVizualization extends React.Component {
         })
       ]
     });
+
     wavesurfer.on('ready', function ()
     {
       document.getElementById('slider').oninput = function()
@@ -82,21 +113,67 @@ class AudioVizualization extends React.Component {
       <div>
         <div id="waveform"></div>
         <div id="timeline"></div>
-        <input id="slider" type="range" min="1" max="200" defaultValue="1"/>
+        <input id="slider" type="range" min="1" max="500" defaultValue="1"/>
         <input type="file" id="file" onChange={this.handleChange} />
-        <p id="startTime">Start time: </p>
-        <p id="endTime">End time: </p>
       </div>
     );
   }
 }
 
 class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    this.handleSaveButton = this.handleSaveButton.bind(this);
+    this.state = {
+      play: false
+    };
+  }
+
+  handleSaveButton()
+  {
+    return;
+  }
+
   render()
   {
     return (
       <div className="App">
-        <AudioVizualization/>
+      <div class="container">
+        <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+          <div class="col-md-12 px-0">
+            <AudioVizualization/>
+          </div>
+        </div>
+
+        <div class="row mb-2">
+          <div class="col-md-6">
+            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+              <div class="card-body d-flex flex-column align-items-start">
+                <p> Dictor info: </p>
+                Name: <input id="dictorName" type="text"/><br/>
+                City: <input id="dictorCity" type="text"/><br/>
+                Country: <input id="dictorCountry" type="text"/><br/>
+                Lang: <input id="dictorLang" type="text"/><br/>
+                Defect: <input id="dictorDefect" type="text"/>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+              <div class="card-body d-flex flex-column align-items-start">
+                <p> Sound info: </p>
+                <p id="startTime">Start time: </p>
+                <p id="endTime">End time: </p>
+                Lang: <input id="dictorLang" type="text"/><br/>
+                Dialect: <input id="dictorLang" type="text"/><br/>
+                Value: <input id="dictorLang" type="text"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button id="saveData" onClick={this.handleSaveButton}>Save</button>
+      </div>
       </div>
     );
   }
