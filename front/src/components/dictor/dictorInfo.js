cfd0641d35a5860bfc854138619e444ba5961cfd
Alexandr Kvasnikov <uselessflower@gmail.com>
2019-12-18 02:23:44 +0300

    style

--- front/src/components/dictor/dictorInfo.js
+++ front/src/components/dictor/dictorInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Select from 'react-select';
+import '../../App.css';
 
 class DictorInfo extends React.Component {  
 	constructor(props)
@@ -38,27 +39,33 @@ class DictorInfo extends React.Component {
 		return (
 			<div className="col-md-12">
 				<div className="row">
-					<div className="col-md-3"></div>
-					<div className="col-md-6">
+					<div className="col-md-2"></div>
+					<div className="col-md-8">
 			            <div className="card flex-md-row mb-4 box-shadow h-md-250">
 			                <div className="card-body d-flex flex-column align-items-start">
-			                  <p> Описание диктора: </p>
-			                  Псевдоним: <input name="dictorPseudo" onChange={this.props.handleInputChange} type="text"/><br/>
-			                  Имя: <input name="dictorName" onChange={this.props.handleInputChange} type="text"/><br/>
-			                  Город: <input name="dictorCity" onChange={this.props.handleInputChange} type="text"/><br/>
-			                  Страна: <input name="dictorCountry" onChange={this.props.handleInputChange} type="text"/><br/>
-			                  Родной язык: <input name="dictorLang" onChange={this.props.handleInputChange} type="text"/><br/>
-			                  Пол: <input name="dictorSex" onChange={this.props.handleInputChange} type="text"/><br/>
-			                  Возраст: <input name="dictorAge" onChange={this.props.handleInputChange} type="text"/><br/>
-			                  Нарушения речи:
-			                  <div id="select">
-			                    {this.renderSelect()}
-			                  </div>
-			                  <button className="btn btn-dark" name="saveDictor" onClick={this.props.saveDictor}>Сохранить</button>
+			                	<div className="inlinediv">
+				                  	<div className="inline">
+				                  		<p> Описание диктора: </p>
+					                  	Псевдоним: <input name="dictorPseudo" onChange={this.props.handleInputChange} type="text"/><br/>
+					                  	Имя: <input name="dictorName" onChange={this.props.handleInputChange} type="text"/><br/>
+					                  	Город: <input name="dictorCity" onChange={this.props.handleInputChange} type="text"/><br/>
+					                </div>
+					                <div className="inline">
+					                  	Страна: <input name="dictorCountry" onChange={this.props.handleInputChange} type="text"/><br/>
+					                  	Родной язык: <input name="dictorLang" onChange={this.props.handleInputChange} type="text"/><br/>
+					                  	Пол: <input name="dictorSex" onChange={this.props.handleInputChange} type="text"/><br/>
+					                  	Возраст: <input name="dictorAge" onChange={this.props.handleInputChange} type="text"/><br/>
+					                </div>
+					            </div>
+				                Нарушения речи:
+			                  	<div id="select">
+			                    	{this.renderSelect()}
+			                  	</div>
+			                  	<button className="btn btn-dark" name="saveDictor" onClick={this.props.saveDictor}>Сохранить</button>
 			            	</div>
               			</div>
               		</div>
-              		<div className="col-md-3"></div>
+              		<div className="col-md-2"></div>
               	</div>
             </div>
 		);
