17c8354fc089f9f9139fd1c8587e0e567e65d063
Alexandr Kvasnikov <uselessflower@gmail.com>
2019-12-18 04:30:15 +0300

    bug

--- front/src/components/post/waveplayer.js
+++ front/src/components/post/waveplayer.js
@@ -3,23 +3,61 @@ import WaveSurfer from 'wavesurfer.js';
 import RegionPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js';
 import Timeline from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js';
 import axios from "axios";
+import Select from 'react-select';
 
 class WavePlayer extends React.Component {  
   constructor(props)
   {
     super(props);
-
     this.changeFile = this.changeFile.bind(this);
     this.createWavePlayer = this.createWavePlayer.bind(this);
+    this.options = [{value:"drum.wav", label:"drum.wav"}, {value:"str.m4a", label:"str.m4a"}];
+    this.state = {selectedOption: {}}
+  }
+
+  renderSelect()
+  {
+    return (
+      <Select
+        id="files"
+        style={{width: '50px'}}
+        name="Файлы"
+        options={this.options}
+        closeMenuOnSelect={true}
+        value={this.state.selectedOption}
+        onChange={
+          (selectedOpt) => {this.changeSelected(selectedOpt)}
+        }
+      />
+    );
+  }
+
+  changeSelected(selectedOpt){
+    document.getElementById('waveform').innerHTML = '';
+
+    var blob = null;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", 'http://speechdb.ru/audio/' + selectedOpt.value);
+    xhr.responseType = "blob";//force the HTTP response, response-type header to be blob
+    xhr.onload = function()
+    {
+        blob = xhr.response;//xhr.response is now a blob object
+        console.log(blob);
+    }
+    xhr.send();
+
+    this.createWavePlayer('http://speechdb.ru/audio/' + selectedOpt.value);
+    this.setState({selectedOption: selectedOpt});
   }
 
   changeFile(e){
+    //console.log(e.target.files[0], URL.createObjectURL(e.target.files[0]));
     this.createWavePlayer(URL.createObjectURL(e.target.files[0]));
+    //this.createWavePlayer('http://speechdb.ru/audio/drum.wav');
   }
 
   createWavePlayer(url)
   {
-    console.log(url);
     let wavesurfer = WaveSurfer.create({
       container: '#waveform',
       waveColor: 'red',
@@ -66,20 +104,22 @@ class WavePlayer extends React.Component {
       region.play();
     });
     wavesurfer.load(url);
+    
   }
 
   render() {  
     return (
-      //<div className="jumbotron p-3 p-md-5 text-white rounded bg-dark zindex">
         <div className="col-md-12 px-0">
           <div id="waveform"></div>
           <div id="timeline"></div>
           <input id="slider" type="range" min="1" max="500" defaultValue="1"/>
+          <div id="select">
+            {this.renderSelect()}
+          </div>
           <input type="file" id="file" name="filetoupload" onChange={this.changeFile} />
           <div style={{display: "none"}}> <input id="prevEnd" value={this.props.state.endTime} type="text" /><input id="prevStart" value={this.props.state.startTime} type="text" /> </div>
           <p></p>
         </div>
-      //</div>
     );
   }
 }
