0c3e33c30422c854d26491bca77266133066b8b1
Stroom <stroooooom@gmail.com>
2020-10-02 19:42:24 +0300

    added new module for working with markdowns in MongoDB

---
+++ server/src/markdownsDB.js
@@ -0,0 +1,52 @@
+var cfg = require('./cfg');
+
+var mongoose = require('mongoose');
+
+var markdownSchema = new mongoose.Schema({
+	username: String,
+	recordname: String,
+	phonemes: [{ _id: Number, start: Number, end: Number, language: String, dialect: String, notation: String }],
+	words: [{ _id: Number, start: Number, end: Number, value: String}],
+	sentences: [{ _id: Number, start: Number, end: Number, value: String}]
+});
+
+async function addMarkdown(markdown) {
+	try {
+		var connection = mongoose.createConnection(cfg.markdowns_db_uri, {useNewUrlParser: true, useUnifiedTopology: true});
+		var MarkdownModel = connection.model('Markdown', markdownSchema);
+
+		var doc = new MarkdownModel(markdown);
+		await doc.save();
+		return { completed: true, output: "Markdown was sucessfully added!" };
+	} catch (err) {
+		return { completed: false, output: err.message };
+	} finally {
+		connection.close();
+	}
+}
+
+async function updateMarkdown(markdown) {
+	try {
+		var connection = mongoose.createConnection(cfg.markdowns_db_uri, {useNewUrlParser: true, useUnifiedTopology: true});
+		var MarkdownModel = connection.model('Markdown', markdownSchema);
+
+		var result = await MarkdownModel.updateOne(
+			{ username: markdown.username, recordname: markdown.recordname }, 
+			{ $set: {
+				phonemes: markdown.phonemes,
+				words: markdown.words,
+				sentences: markdown.sentences } 
+			})
+		if (result.n == 0) {
+			throw new Error('No documents were matched');
+		}
+		return { completed: true, output: "Markdown was sucessfully updated!" };
+	} catch (err) {
+		return { completed: false, output: err.message };
+	} finally {
+		connection.close();
+	}
+}
+
+exports.addMarkdown = addMarkdown;
+exports.updateMarkdown = updateMarkdown;
