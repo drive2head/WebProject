d5772526205608bbc1adc5d1b8aa2e02b9d3142d
Nikolai Slavnov <n.slavnov@kod-it.ru>
2020-11-27 02:20:01 +0300

    needed for successful unit test passing

--- authService/build/service.js
+++ authService/build/service.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -29,16 +10,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteUser = exports.addUser = exports.getUser = exports.verifyUser = void 0;
-const mongoose = __importStar(require("mongoose"));
 const cfg_1 = require("./cfg");
-const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    name: String,
-    surname: String,
-});
+const mongoose_1 = require("mongoose");
+const model_1 = require("./model");
 function connect() {
-    return mongoose.createConnection(cfg_1.users_db_uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    return mongoose_1.createConnection(cfg_1.users_db_uri, { useNewUrlParser: true, useUnifiedTopology: true });
 }
 function verifyUser(username, password) {
     return __awaiter(this, void 0, void 0, function* () {
@@ -73,7 +49,7 @@ function getUser(username) {
     return __awaiter(this, void 0, void 0, function* () {
         const users_connection = connect();
         try {
-            const User = users_connection.model('User', userSchema);
+            const User = users_connection.model('User', model_1.userSchema);
             return yield User.findOne({ username: username });
         }
         catch (err) {
@@ -97,7 +73,7 @@ function addUser(username, password, name, surname) {
     return __awaiter(this, void 0, void 0, function* () {
         const users_connection = connect();
         try {
-            const User = users_connection.model('User', userSchema);
+            const User = users_connection.model('User', model_1.userSchema);
             const userExists = yield getUser(username);
             if (userExists) {
                 return null;
@@ -124,7 +100,7 @@ function deleteUser(username) {
     return __awaiter(this, void 0, void 0, function* () {
         const users_connection = connect();
         try {
-            const User = users_connection.model('User', userSchema);
+            const User = users_connection.model('User', model_1.userSchema);
             const userExists = yield getUser(username);
             if (!userExists) {
                 return { completed: false, output: `User '${username}' doesn't exist` };
