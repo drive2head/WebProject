27d3e983eea5f5ac883bfe4469b056daef224f94
Nikolai Slavnov <n.slavnov@kod-it.ru>
2020-11-29 16:58:10 +0300

    bug fix

--- audioService/model.ts
+++ audioService/model.ts
@@ -1,9 +1,8 @@
 import * as mongoose from "mongoose";
-import {Schema} from "mongoose";
+import {Schema, Types} from "mongoose";
 
 export const recordSchema = new Schema({
     path: String,
     name: String,
-    speakerId: mongoose.Types.ObjectId,
+    speakerId: Types.ObjectId,
 });
-export const Record = mongoose.model("users", recordSchema);
