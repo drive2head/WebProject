d38ebf545659c85d93f70991dc2a3a230eee4c62
Nikolai Slavnov <n.slavnov@kod-it.ru>
2021-01-14 21:08:35 +0300

    little fix

--- server/src/CB.js
+++ server/src/CB.js
@@ -16,8 +16,8 @@ class CB {
     timeout = 2000         // время ожидания выполнения запроса
     failure_threshold = 5  // количество ошибок, после которого сервер меняет состояние
     success_threshold = 5  // количество запросов, которые должны пройти успешно, для возвращения в CLOSED
-    time_threshold = 60000 // время, в течении которого подсчитывается количество ошибок
-    waiting_time = 60000   // время ожидания (между переходом из OPENED в HALFOPENED)
+    time_threshold = 20000 // время, в течении которого подсчитывается количество ошибок
+    waiting_time = 20000   // время ожидания (между переходом из OPENED в HALFOPENED)
 
     constructor(serverAddr) {
         console.log(this)
@@ -25,43 +25,54 @@ class CB {
         if (this._timer != null)
             clearInterval(this._timer);
         // Устанавливаем планировщик, которые каждые $time_threshold мс сбрасывает счетчик ошибок
-        this._timer = setInterval(() => { this._failure_counter = 0; }, this.time_threshold);
+        this._timer = setInterval(() => { 
+            this._failure_counter = 0; 
+            console.log("Failure counter is reset!");
+        }, this.time_threshold);
+        this.close();
     }
 
     _sendRequest = async (req) => {
-        console.log("this:", this);
         return await axios({
             method: req.method,
-            url: path.join(this._serverAddr, req.url),
+            url: this._serverAddr + req.url,
             data: req.body
         });
     };
 
-    _sendError = (res) => {
-        res.status(503).send({ service: "CircuitBraker", message: "Service is unavailiable" });
+    _sendError = (res, statusCode = null, statusText = null) => {
+        if (statusCode != null) {
+            res.status(statusCode).send({ message: statusText });
+        } else {
+            res.status(503).send({ service: "CircuitBraker", message: "Service is unavailiable" });
+        }
     };
 
     _sendResult = (res, result) => {
-        res.status(200).send(result); // TODO: проверить, нужно ли отправлять весь result
+        res.status(200).send(result.data);
     };
 
     fetch = async (req, res) => {
-        console.log("this._state:", this._state);
-        console.log("this._serverAddr:", this._serverAddr);
+        console.log("state:", this._state);
+
         switch (this._state) {
             case CLOSED:
                 this._sendRequest(req)
                 .then(result => {
-                    console.log("RESULT:\n", result);
                     this._sendResult(res, result);
                 })
                 .catch(error => {
-                    console.log("ERROR:\n", error);
-                    this._failure_counter += 1;
-                    if (this._failure_counter >= this.failure_threshold) {
-                        this.open();
+                    if (error.response.status != undefined && error.response.status >= 500) {
+                        console.log("CLOSED: error occured");
+                        this._failure_counter += 1;
+                        console.log(`Counted ${this._failure_counter} of ${this.failure_threshold} failures`);
+                        if (this._failure_counter >= this.failure_threshold) {
+                            this.open();
+                        }
+                        this._sendError(res);
+                    } else {
+                        this._sendError(res, error.response.status, error.response.statusText);
                     }
-                    // this._sendError(res);
                 });
                 break;
             case OPENED:
@@ -70,15 +81,15 @@ class CB {
             case HALFOPENED:
                 this._sendRequest(req)
                 .then(result => {
-                    console.log("RESULT:\n", result);
                     this._success_counter += 1;
-                    if (this._success_counter >= success_threshold) {
+                    console.log(`Counted ${this._success_counter} of ${this.success_threshold} successes`);
+                    if (this._success_counter >= this.success_threshold) {
                         this.close();
                     }
                     this._sendResult(res, result);
                 })
                 .catch(error => {
-                    console.log("ERROR:\n", error);
+                    console.log("HALFOPENED: error occured");
                     this.open();
                     this._sendError(res);
                 })
@@ -89,16 +100,19 @@ class CB {
     }
 
     open = () => {
+        console.log("OPENED!");
         this._state = OPENED;
         setTimeout(this.halfopen, this.waiting_time);
     }
 
     halfopen = () => {
+        console.log("HALFOPENED!");
         this._success_counter = 0;
         this._state = HALFOPENED;
     }
 
     close = () => {
+        console.log("CLOSED!");
         this._failure_counter = 0;
         this._state = CLOSED;
     }
